823aabee5840ab3f1e30d0fe7d4915ae1a076c68
Ian Macartney <ian@convex.dev>
2023-01-23 10:39:21 -0800

    double quote

--- convex/lib/withUser.ts
+++ convex/lib/withUser.ts
@@ -1,5 +1,5 @@
-import { QueryCtx, MutationCtx, mutation, query } from '../_generated/server';
-import { Document } from '../_generated/dataModel';
+import { QueryCtx, MutationCtx, mutation, query } from "../_generated/server";
+import { Document } from "../_generated/dataModel";
 
 /**
  * Wrapper for a Convex query or mutation function that provides a user in ctx.
@@ -12,7 +12,7 @@ import { Document } from '../_generated/dataModel';
  */
 export const withUser = <Ctx extends QueryCtx, Args extends any[], Output>(
   func: (
-    ctx: Ctx & { user: Document<'users'> },
+    ctx: Ctx & { user: Document<"users"> },
     ...args: Args
   ) => Promise<Output>
 ): ((ctx: Ctx, ...args: Args) => Promise<Output>) => {
@@ -20,7 +20,7 @@ export const withUser = <Ctx extends QueryCtx, Args extends any[], Output>(
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new Error(
-        'Unauthenticated call to function requiring authentication'
+        "Unauthenticated call to function requiring authentication"
       );
     }
     // Note: If you don't want to define an index right away, you can use
@@ -28,13 +28,13 @@ export const withUser = <Ctx extends QueryCtx, Args extends any[], Output>(
     //  .filter(q => q.eq(q.field("tokenIdentifier"), identity.tokenIdentifier))
     //  .unique();
     const user = await ctx.db
-      .query('users')
-      .withIndex('by_token', (q) =>
-        q.eq('tokenIdentifier', identity.tokenIdentifier)
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
       )
       .unique();
-    if (!user) throw new Error('User not found');
-    return await func({ ...ctx, user }, ...args);
+    if (!user) throw new Error("User not found");
+    return func({ ...ctx, user }, ...args);
   };
 };
 
@@ -50,7 +50,7 @@ export const withUser = <Ctx extends QueryCtx, Args extends any[], Output>(
 export const mutationWithUser = <Args extends any[], Output>(
   func: (
     ctx: MutationCtx & {
-      user: Document<'users'>;
+      user: Document<"users">;
     },
     ...args: Args
   ) => Promise<Output>
@@ -69,7 +69,7 @@ export const mutationWithUser = <Args extends any[], Output>(
  */
 export const queryWithUser = <Args extends any[], Output>(
   func: (
-    ctx: QueryCtx & { user: Document<'users'> },
+    ctx: QueryCtx & { user: Document<"users"> },
     ...args: Args
   ) => Promise<Output>
 ) => {
