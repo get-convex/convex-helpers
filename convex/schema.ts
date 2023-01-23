57da440804cef6f418bfb5554a23a23030dd3234
Ian Macartney <ian@convex.dev>
2023-01-23 15:25:31 -0800

    copy typescript tutorial for helpers

---
+++ convex/schema.ts
@@ -0,0 +1,22 @@
+import { defineSchema, defineTable, s } from "convex/schema";
+
+export default defineSchema({
+  // For withUser:
+  users: defineTable({
+    name: s.string(),
+    tokenIdentifier: s.string(),
+  }).index("by_token", ["tokenIdentifier"]),
+  // End withUser
+  // For presence:
+  presence: defineTable({
+    user: s.string(),
+    room: s.string(),
+    updated: s.number(),
+    data: s.any(),
+  })
+    // Index for fetching presence data
+    .index("by_room_updated", ["room", "updated"])
+    // Index for updating presence data
+    .index("by_user_room", ["user", "room"]),
+  //End presence
+});
