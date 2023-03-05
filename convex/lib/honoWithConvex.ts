62bddaeff8af4ad86f4d304db1d7718cd9b95818
Sarah Shader <sarah@convex.dev>
2023-03-05 11:41:48 -0500

    Update honoWithConvex.ts

--- convex/lib/honoWithConvex.ts
+++ convex/lib/honoWithConvex.ts
@@ -1,15 +1,65 @@
-import { HttpRouter, PublicHttpEndpoint, RoutableMethod } from "convex/server";
+/**
+ * Use these helpers to use Hono for routing and middleware in Convex.
+ * In `convex/http.ts`:
+ * ```ts
+ * const app: HonoWithConvex = new Hono();
+ * // add Hono routes on `app`
+ * export default new HttpRouterWithHono(app);
+ * ```
+ */
+import {
+  HttpRouter,
+  PublicHttpEndpoint,
+  RoutableMethod,
+  ROUTABLE_HTTP_METHODS,
+} from "convex/server";
 import { Hono } from "hono";
 import { httpEndpoint, HttpEndpointCtx } from "../_generated/server";
 
-// Idk where this type is supposed to come from but hack to get it working
+/**
+ * Hono uses the `FetchEvent` type internally, which has to do with service workers
+ * and isn't included in the Convex tsconfig.
+ *
+ * As a workaround, define this type here so Hono + Convex compiles.
+ */
 declare global {
   type FetchEvent = any;
 }
 
-// So `c.env.runQuery` can work
+/**
+ * A type representing a Hono app with `c.env` containing Convex's
+ * `HttpEndpointCtx` (e.g. `c.env.runQuery` is valid).
+ */
 export type HonoWithConvex = Hono<{ Bindings: HttpEndpointCtx }>;
 
+/**
+ * An implementation of the Convex `HttpRouter` that integrates with Hono by
+ * overridding `getRoutes` and `lookup`.
+ *
+ * This defers all routing and request handling to the provided Hono app, and
+ * passes along the Convex `HttpEndpointCtx` to the Hono handlers as part of
+ * `env`.
+ *
+ * It will attempt to log each request with the most specific Hono route it can
+ * find. For example,
+ *
+ * ```
+ * app.on("GET", "*", ...)
+ * app.on("GET", "/profile/:userId", ...)
+ *
+ * const http = new HttpRouterWithHono(app);
+ * http.lookup("/profile/abc", "GET") // [handler, "GET", "/profile/:userId"]
+ * ```
+ *
+ * An example `convex/http.ts` file would look like this:
+ * ```
+ * const app: HonoWithConvex = new Hono();
+ *
+ * // add Hono routes on `app`
+ *
+ * export default new HttpRouterWithHono(app);
+ * ```
+ */
 export class HttpRouterWithHono extends HttpRouter {
   private _app: HonoWithConvex;
   private _handler: PublicHttpEndpoint<any>;
@@ -26,35 +76,65 @@ export class HttpRouterWithHono extends HttpRouter {
     this._handlerInfoCache = new Map();
   }
 
-  // Used to populate the list of routes on the Functions page of the dashboard
+  /**
+   * Returns a list of routed HTTP endpoints.
+   *
+   * These are used to populate the list of routes shown in the Functions page of the Convex dashboard.
+   *
+   * @returns - an array of [path, method, endpoint] tuples.
+   */
   getRoutes = () => {
-    const convexRoutes = this._app.routes.map((route) => {
-      return [route.path, route.method as RoutableMethod] as const;
-    });
-    const result = [];
-    const seen = new Set();
+    const convexRoutes: [string, RoutableMethod, (...args: any) => any][] = [];
+
     // Likely a better way to do this, but hono will have multiple handlers with the same
     // name (i.e. for middleware), so de-duplicate so we don't show multiple routes in the dashboard.
-    // Also source mapping does not play well with this.
-    for (const route of convexRoutes) {
-      const name = `${route[1]} ${route[0]}`;
-      if (seen.has(name)) {
-        continue;
+    const seen = new Set();
+    this._app.routes.forEach((route) => {
+      // Hono uses "ALL" in its router, which is not supported by the Convex router.
+      // Expand this into a route for every routable method supported by Convex.
+      if (route.method === "ALL") {
+        for (const method of ROUTABLE_HTTP_METHODS) {
+          const name = `${method} ${route.path}`;
+          if (!seen.has(name)) {
+            seen.add(name);
+            convexRoutes.push([route.path, method, route.handler]);
+          }
+        }
       } else {
-        seen.add(name);
-        result.push([route[0], route[1], this._handler] as const);
+        const name = `${route.method} ${route.path}`;
+        if (!seen.has(name)) {
+          seen.add(name);
+          convexRoutes.push([
+            route.path,
+            route.method as RoutableMethod,
+            route.handler,
+          ]);
+        }
       }
-    }
-    return result;
+    });
+    return convexRoutes;
   };
 
-  // Used to attribute a request with a particular endpoint for the purposes of
-  // metrics and logs. The method and path should match one of the routes returned
-  // by `getRoutes`
+  /**
+   * Returns the appropriate HTTP endpoint and its routed request path and method.
+   *
+   * The path and method returned are used for logging and metrics, and should
+   * match up with one of the routes returned by `getRoutes`.
+   *
+   * For example,
+   *
+   * ```js
+   * http.route({ pathPrefix: "/profile/", method: "GET", handler: getProfile});
+   *
+   * http.lookup("/profile/abc", "GET") // returns [getProfile, "GET", "/profile/*"]
+   *```
+   *
+   * @returns - a tuple [PublicHttpEndpoint, method, path] or null.
+   */
   lookup = (path: string, method: RoutableMethod | "HEAD") => {
     const match = this._app.router.match(method, path);
     if (match === null) {
-      return [this._handler, method as RoutableMethod, path] as const;
+      return [this._handler, normalizeMethod(method), path] as const;
     }
     // There might be multiple handlers for a route (in the case of middleware),
     // so choose the most specific one for the purposes of logging
@@ -63,7 +143,7 @@ export class HttpRouterWithHono extends HttpRouter {
     if (this._handlerInfoCache.size === 0) {
       for (const r of this._app.routes) {
         this._handlerInfoCache.set(r.handler, {
-          method: r.method as RoutableMethod,
+          method: normalizeMethod(method),
           path: r.path,
         });
       }
@@ -73,6 +153,14 @@ export class HttpRouterWithHono extends HttpRouter {
       return [this._handler, info.method, info.path] as const;
     }
 
-    return [this._handler, method as RoutableMethod, path] as const;
+    return [this._handler, normalizeMethod(method), path] as const;
   };
+}
+
+export function normalizeMethod(
+  method: RoutableMethod | "HEAD"
+): RoutableMethod {
+  // HEAD is handled by Convex by running GET and stripping the body.
+  if (method === "HEAD") return "GET";
+  return method;
 }
