8ceba36067811f72435358dc49a3e319c0d6073b
Ian Macartney <ianhmacartney@gmail.com>
2023-11-28 18:20:33 -0800

    use rls from counter

--- convex/counter.ts
+++ convex/counter.ts
@@ -1,5 +1,7 @@
-import { query } from "./_generated/server";
+import { DataModel } from "./_generated/dataModel";
+import { DatabaseReader, query } from "./_generated/server";
 import { mutation } from "./_generated/server";
+import { RowLevelSecurity } from "convex-helpers/server/rowLevelSecurity";
 
 const getCounter = query(
   async (ctx, { counterName }: { counterName: string }): Promise<number> => {
@@ -11,25 +13,44 @@ const getCounter = query(
   }
 );
 
+const { withMutationRLS, withQueryRLS } = RowLevelSecurity<
+  { db: DatabaseReader },
+  DataModel
+>({
+  counter_table: {
+    insert: async () => {
+      return true;
+    },
+    read: async () => {
+      return true;
+    },
+    modify: async () => {
+      return true;
+    },
+  },
+});
+
 const incrementCounter = mutation(
-  async (
-    ctx,
-    { counterName, increment }: { counterName: string; increment: number }
-  ) => {
-    const counterDoc = await ctx.db
-      .query("counter_table")
-      .filter((q) => q.eq(q.field("name"), counterName))
-      .first();
-    if (counterDoc === null) {
-      await ctx.db.insert("counter_table", {
-        name: counterName,
-        counter: increment,
-      });
-    } else {
-      counterDoc.counter += increment;
-      await ctx.db.replace(counterDoc._id, counterDoc);
+  withMutationRLS(
+    async (
+      ctx,
+      { counterName, increment }: { counterName: string; increment: number }
+    ) => {
+      const counterDoc = await ctx.db
+        .query("counter_table")
+        .filter((q) => q.eq(q.field("name"), counterName))
+        .first();
+      if (counterDoc === null) {
+        await ctx.db.insert("counter_table", {
+          name: counterName,
+          counter: increment,
+        });
+      } else {
+        counterDoc.counter += increment;
+        await ctx.db.replace(counterDoc._id, counterDoc);
+      }
     }
-  }
+  )
 );
 
 export { getCounter, incrementCounter };
