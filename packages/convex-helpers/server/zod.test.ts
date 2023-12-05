db12e672fcafc9f4a715c51be68e0d897385bfc4
Ian Macartney <ianhmacartney@gmail.com>
2023-12-05 04:13:55 -0800

    returns wip

---
+++ packages/convex-helpers/server/zod.test.ts
@@ -0,0 +1,170 @@
+import { zCustomQuery } from "./zod";
+import { z } from "zod";
+import { v } from "convex/values";
+import { useQuery } from "convex/react";
+import { ApiFromModules, queryGeneric as query } from "convex/server";
+
+/**
+ * Adding ctx
+ */
+const addCtxArg = zCustomQuery(query, {
+  args: {},
+  input: async (ctx) => {
+    return { ctx: { ...ctx, a: "hi" }, args: {} };
+  },
+});
+const addC = addCtxArg({
+  args: {},
+  handler: async (ctx) => {
+    return { ctxA: ctx.a }; // !!!
+  },
+});
+const addCtxResult = useQuery(api.test.addC, { z: "yo" });
+console.log(addCtxResult?.ctxA);
+/**
+ * Adding arg
+ */
+const addArg = zCustomQuery(query, {
+  args: {},
+  input: async (ctx, {}) => {
+    return { ctx, args: { a: "hi" } };
+  },
+});
+const add = addArg({
+  args: {},
+  handler: async (_ctx, args) => {
+    return { argsA: args.a }; // !!!
+  },
+});
+const addResult = useQuery(api.test.add, { z: "yo" });
+console.log(addResult?.argsA);
+/**
+ * Consuming arg, add to ctx
+ */
+const consumeArg = zCustomQuery(query, {
+  args: { a: v.string() },
+  input: async (ctx, { a }) => {
+    return { ctx: { ...ctx, a }, args: {} };
+  },
+});
+const consume = consumeArg({
+  args: {},
+  handler: async (ctx, emptyArgs) => {
+    assert<Equals<typeof emptyArgs, {}>>(); // !!!
+    return { ctxA: ctx.a };
+  },
+});
+const consumeResult = useQuery(api.test.consume, { a: "hi", z: "yo" });
+console.log(consumeResult?.ctxA);
+/**
+ * Passing Through arg, also add to ctx for fun
+ */
+const passThrougArg = zCustomQuery(query, {
+  args: { a: v.string() },
+  input: async (ctx, args) => {
+    return { ctx: { ...ctx, a: args.a }, args };
+  },
+});
+const passThrough = passThrougArg({
+  args: {},
+  handler: async (ctx, args) => {
+    return { ctxA: ctx.a, argsA: args.a }; // !!!
+  },
+});
+const passThroughResult = useQuery(api.test.passThrough, { a: "hi", z: "yo" });
+console.log(passThroughResult?.ctxA, passThroughResult?.argsA);
+/**
+ * Modify arg type, don't need to re-defined "a" arg
+ */
+const modifyArg = zCustomQuery(query, {
+  args: { a: v.string() },
+  input: async (ctx, { a }) => {
+    return { ctx: { ...ctx, a }, args: { a: 123 } }; // !!!
+  },
+});
+const modify = modifyArg({
+  args: {},
+  handler: async (ctx, args) => {
+    args.a.toFixed; // !!!
+    return { ctxA: ctx.a, argsA: args.a };
+  },
+});
+const modifyResult = useQuery(api.test.modify, { a: "hi", z: "yo" });
+console.log(modifyResult?.ctxA.charAt, modifyResult?.argsA.toFixed); // !!!
+
+/**
+ * Redefine arg type with the same type: OK!
+ */
+const redefineArg = zCustomQuery(query, {
+  args: { a: v.string() },
+  input: async (ctx, args) => {
+    return { ctx, args };
+  },
+});
+const redefine = redefineArg({
+  args: { a: z.string() },
+  handler: async (_ctx, args) => {
+    return { argsA: args.a };
+  },
+});
+const redefineResult = useQuery(api.test.redefine, { a: "hi", z: "yo" });
+console.log(redefineResult?.argsA.charAt);
+/**
+ * Redefine arg type with different type: error!
+ */
+const badRedefineArg = zCustomQuery(query, {
+  args: { a: v.string(), b: v.number() },
+  input: async (ctx, args) => {
+    return { ctx, args };
+  },
+});
+const badRedefine = badRedefineArg({
+  args: { a: z.number() },
+  handler: async (_ctx, args) => {
+    assert<Equals<typeof args.a, never>>(); // !!!
+    return { argsA: args.a };
+  },
+});
+const never: never = null as never;
+// Errors if you pass a string to "a".
+// One caveat is that if you don't have a second param, it's ok passing no
+// params ({a: never} seems to type check as {} which means optional params)
+const badRedefineResult = useQuery(api.test.badRedefine, {
+  b: 3,
+  a: never,
+  z: "yo",
+});
+console.log(badRedefineResult?.argsA);
+
+/**
+ * Test helpers
+ */
+
+declare const api: ApiFromModules<{
+  test: {
+    badRedefine: typeof badRedefine;
+    redefine: typeof redefine;
+    modify: typeof modify;
+    consume: typeof consume;
+    passThrough: typeof passThrough;
+    add: typeof add;
+    addC: typeof addC;
+  };
+}>;
+/**
+ * Tests if two types are exactly the same.
+ * Taken from https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
+ * (Apache Version 2.0, January 2004)
+ */
+
+export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? true
+  : false;
+
+export function assert<T extends true>() {
+  // no need to do anything! we're just asserting at compile time that the type
+  // parameter is true.
+  return true as T;
+}
---
+++ packages/convex-helpers/server/zod.ts
@@ -0,0 +1,183 @@
+import { z } from "zod";
+import { Mod, customQuery, splitArgs } from "./mod";
+import { ObjectType, PropertyValidators, Value } from "convex/values";
+import {
+  FunctionVisibility,
+  GenericDataModel,
+  GenericQueryCtx,
+  QueryBuilder,
+  RegisteredQuery,
+  queryGeneric,
+} from "convex/server";
+import { v } from "convex/values";
+export type ZodValidator = Record<string, z.ZodTypeAny>;
+type ZodFn<Ctx, Args extends ZodValidator, Returns> = {
+  args: Args;
+  handler: (ctx: Ctx, arg: z.output<z.ZodObject<Args>>) => Promise<Returns>;
+};
+
+function zQueryCustom<
+  Args extends ZodValidator,
+  Returns,
+  Visibility extends FunctionVisibility,
+  DataModel extends GenericDataModel
+>(
+  _visibility: Visibility,
+  fn: ZodFn<GenericQueryCtx<DataModel>, Args, Returns>
+) {
+  const zodArgs = z.object(fn.args);
+  const argsValidator = { a: v.string() };
+  const query = customQuery(
+    queryGeneric as QueryBuilder<DataModel, Visibility>,
+    {
+      args: argsValidator,
+      input: async (ctx, args) => ({ ctx, args: zodArgs.parse(args) }),
+    }
+  );
+  return query({
+    args: {}, // all added by customQuery
+    handler: async (ctx, args) => {
+      return fn.handler(ctx, args);
+    },
+  });
+}
+const argsValidator = { a: v.string() };
+
+/**
+ * Doesn't work well: doesn't translate things through
+ * @param args
+ * @returns
+ */
+export const zodMod = <
+  Ctx extends Record<string, any>,
+  Args extends ZodValidator
+>(
+  args: Args
+): Mod<Ctx, typeof argsValidator, Ctx, z.output<z.ZodObject<Args>>> => {
+  const zodArgs = z.object(args);
+  return {
+    args: argsValidator,
+    input: async (ctx: Ctx, args: ObjectType<typeof argsValidator>) => ({
+      ctx,
+      args: zodArgs.parse(args),
+    }),
+  };
+};
+
+export function zCustomQuery<
+  ModArgsValidator extends PropertyValidators,
+  ModCtx extends Record<string, any>,
+  ModMadeArgs extends Record<string, any>,
+  Visibility extends FunctionVisibility,
+  DataModel extends GenericDataModel
+>(
+  // _visibility: Visibility,
+  query: QueryBuilder<DataModel, Visibility>,
+  mod: Mod<GenericQueryCtx<DataModel>, ModArgsValidator, ModCtx, ModMadeArgs>
+) {
+  const argsValidator = { z: v.string() };
+  function customQuery<
+    ExistingArgsValidator extends ZodValidator,
+    ReturnsValidator extends ZodValidator,
+    ReturnsValidator extends z.ZodObject = ReturnsValidator extends never
+      ? infer Output
+      : z.output<z.ZodObject<ReturnsValidator>>
+  >(fn: {
+    args: ExistingArgsValidator;
+    handler: (
+      ctx: ModCtx,
+      args: z.output<z.ZodObject<ExistingArgsValidator>> & ModMadeArgs
+    ) => Output | Promise<Output>;
+    returns?: ReturnsValidator;
+  }): RegisteredQuery<
+    Visibility,
+    // z.input<z.ZodObject<ExistingArgsValidator>>
+    ObjectType<typeof argsValidator & ModArgsValidator>,
+    Promise<Output /* adjust output if exists */>
+  > {
+    const zodArgs = z.object(fn.args);
+    return query({
+      args: {
+        ...argsValidator,
+        ...mod.args,
+      } as typeof argsValidator & ModArgsValidator,
+      handler: async (ctx: GenericQueryCtx<DataModel>, allArgs: any) => {
+        const [split, rest] = splitArgs(mod.args, allArgs);
+        // TODO: handle optional input
+        const { ctx: modCtx, args: modArgs } = await mod.input(ctx, split);
+        const output = await fn.handler(modCtx, {
+          ...zodArgs.parse(rest),
+          ...modArgs,
+        });
+        if (fn.returns) {
+          return z.object(fn.returns).parse(output);
+        }
+        return output;
+      },
+    });
+  }
+
+  return customQuery;
+}
+function zQuery<
+  Args extends ZodValidator,
+  Returns,
+  Visibility extends FunctionVisibility,
+  DataModel extends GenericDataModel
+>(
+  // _visibility: Visibility,
+  query2: QueryBuilder<DataModel, Visibility>,
+  fn: ZodFn<GenericQueryCtx<DataModel>, Args, Returns>
+) {
+  const zodArgs = z.object(fn.args);
+  const argsValidator = { a: v.string() };
+  // const query = customQuery(
+  //   queryGeneric as QueryBuilder<DataModel, Visibility>,
+  //   {
+  //     args: argsValidator,
+  //     input: async (ctx, args) => ({ ctx, args: zodArgs.parse(args) }),
+  //   }
+  // );
+  return query2({
+    args: argsValidator,
+    handler: async (ctx, args) => {
+      return fn.handler(ctx, zodArgs.parse(args));
+    },
+  });
+}
+
+const simple = zQuery(queryGeneric, {
+  args: { a: z.custom<Error>(() => new Error()) },
+  handler: async (ctx, { a }) => {
+    ctx.db;
+    a;
+    return 123 as const;
+  },
+});
+
+const simpleQ = useQuery(api.test.simple, { a: "hi" });
+console.log(simpleQ?.toFixed);
+
+import { useQuery } from "convex/react";
+import { ApiFromModules } from "convex/server";
+declare const api: ApiFromModules<{
+  test: {
+    simple: typeof simple;
+  };
+}>;
+
+export const withZod = <Ctx, Args extends ZodValidator, Returns>({
+  args,
+  handler,
+}: {
+  args: Args;
+  handler: (ctx: Ctx, arg: z.output<z.ZodObject<Args>>) => Promise<Returns>;
+}): ((ctx: Ctx, args: z.input<z.ZodObject<Args>>) => Promise<Returns>) => {
+  const zodType = z.function(z.tuple([z.object(args)]));
+  return (ctx, args) => {
+    const innerFunc = (validatedArgs: z.output<z.ZodObject<Args>>) =>
+      handler(ctx, validatedArgs);
+
+    return zodType.implement(innerFunc)(args);
+  };
+};
