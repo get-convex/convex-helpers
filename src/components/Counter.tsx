e9506806b7dc5c54bd07b253fc65683033fcf68c
Ian Macartney <ianhmacartney@gmail.com>
2023-12-08 17:00:29 -0800

    sessionMutation

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,12 @@
 import { api } from "../../convex/_generated/api";
 import { useQuery, useMutation } from "convex/react";
 import { useCallback } from "react";
+import { useSessionMutation } from "../hooks/useServerSession";
 
 const Counter = () => {
   const counter =
     useQuery(api.counter.getCounter, { counterName: "clicks" }) ?? 0;
-  const increment = useMutation(api.counter.incrementCounter);
+  const increment = useSessionMutation(api.counter.incrementCounter);
   const incrementByOne = useCallback(
     () => increment({ counterName: "clicks", increment: 1 }),
     [increment]
