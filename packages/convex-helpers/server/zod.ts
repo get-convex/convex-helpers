1c8e23dd31f2a971b9564481476f8d00227207d7
Ian Macartney <ianhmacartney@gmail.com>
2023-12-11 20:12:03 -0800

    add mutation & action

--- packages/convex-helpers/server/zod.ts
+++ packages/convex-helpers/server/zod.ts
@@ -8,12 +8,16 @@ import {
 import {
   FunctionVisibility,
   GenericDataModel,
+  GenericActionCtx,
   GenericQueryCtx,
+  MutationBuilder,
   QueryBuilder,
   RegisteredAction,
   RegisteredMutation,
   RegisteredQuery,
   UnvalidatedFunction,
+  GenericMutationCtx,
+  ActionBuilder,
 } from "convex/server";
 import { v } from "convex/values";
 import { Mod, NoOp, Registration, splitArgs } from "./customFunctions";
@@ -152,6 +156,246 @@ export function zCustomQuery<
     ModCtx,
     ModMadeArgs,
     GenericQueryCtx<DataModel>,
+    Visibility
+  >;
+}
+
+/**
+ * zCustomMutation is like customMutation, but allows validation via zod.
+ * You can define custom behavior on top of `mutation` or `internalMutation`
+ * by passing a function that modifies the ctx and args. Or NoOp to do nothing.
+ *
+ * Example usage:
+ * ```js
+ * const myMutationBuilder = zCustomMutation(mutation, {
+ *   args: { sessionId: v.id("sessions") },
+ *   input: async (ctx, args) => {
+ *     const user = await getUserOrNull(ctx);
+ *     const session = await db.get(sessionId);
+ *     const db = wrapDatabaseReader({ user }, ctx.db, rlsRules);
+ *     return { ctx: { db, user, session }, args: {} };
+ *   },
+ * });
+ *
+ * // Using the custom builder
+ * export const getSomeData = myMutationBuilder({
+ *   args: { someArg: z.string() },
+ *   handler: async (ctx, args) => {
+ *     const { db, user, session, scheduler } = ctx;
+ *     const { someArg } = args;
+ *     // ...
+ *   }
+ * });
+ * ```
+ *
+ * Simple usage only modifying ctx:
+ * ```js
+ * const myInternalMutation = zCustomMutation(
+ *   internalMutation,
+ *   customCtx(async (ctx) => {
+ *     return {
+ *       // Throws an exception if the user isn't logged in
+ *       user: await getUserByTokenIdentifier(ctx),
+ *     };
+ *   })
+ * );
+ *
+ * // Using it
+ * export const getUser = myInternalMutation({
+ *   args: { email: z.string().email() },
+ *   handler: async (ctx, args) => {
+ *     console.log(args.email);
+ *     return ctx.user;
+ *   },
+ * });
+ *
+ * @param mutation The mutation to be modified. Usually `mutation` or `internalMutation`
+ *   from `_generated/server`.
+ * @param mod The modifier to be applied to the mutation, changing ctx and args.
+ * @returns A new mutation builder using zod validation to define queries.
+ */
+export function zCustomMutation<
+  ModArgsValidator extends PropertyValidators,
+  ModCtx extends Record<string, any>,
+  ModMadeArgs extends Record<string, any>,
+  Visibility extends FunctionVisibility,
+  DataModel extends GenericDataModel
+>(
+  mutation: MutationBuilder<DataModel, Visibility>,
+  mod: Mod<GenericMutationCtx<DataModel>, ModArgsValidator, ModCtx, ModMadeArgs>
+) {
+  // Looking forward to when input / args / ... are optional
+  const inputMod = mod.input ?? NoOp.input;
+  const inputArgs = mod.args ?? NoOp.args;
+  function customMutationBuilder(fn: any): any {
+    if ("args" in fn) {
+      const convexValidator = zodToConvex(fn.args);
+      return mutation({
+        args: {
+          ...convexValidator,
+          ...inputArgs,
+        },
+        handler: async (ctx, allArgs: any) => {
+          const { split, rest } = splitArgs(inputArgs, allArgs);
+          const added = await inputMod(ctx, split);
+          try {
+            const validated = z.object(fn.args).parse(rest);
+            return await fn.handler(
+              { ...ctx, ...added.ctx },
+              { ...validated, ...added.args }
+            );
+          } catch (e) {
+            if (e instanceof z.ZodError) {
+              throw new ConvexError({
+                ZodError: JSON.parse(JSON.stringify(e.errors, null, 2)),
+              });
+            }
+            throw e;
+          }
+        },
+      });
+    }
+    if (Object.keys(inputArgs).length > 0) {
+      throw new Error(
+        "If you're using a custom function with arguments for the input " +
+          "modifier, you must declare the arguments for the function too."
+      );
+    }
+    const handler = fn.handler ?? fn;
+    return mutation({
+      handler: async (ctx, args: any) => {
+        const { ctx: modCtx } = await inputMod(ctx, args);
+        return await handler({ ...ctx, ...modCtx }, args);
+      },
+    });
+  }
+
+  return customMutationBuilder as CustomBuilder<
+    "mutation",
+    ModArgsValidator,
+    ModCtx,
+    ModMadeArgs,
+    GenericMutationCtx<DataModel>,
+    Visibility
+  >;
+}
+
+/**
+ * zCustomAction is like customAction, but allows validation via zod.
+ * You can define custom behavior on top of `action` or `internalAction`
+ * by passing a function that modifies the ctx and args. Or NoOp to do nothing.
+ *
+ * Example usage:
+ * ```js
+ * const myActionBuilder = zCustomAction(action, {
+ *   args: { sessionId: v.id("sessions") },
+ *   input: async (ctx, args) => {
+ *     const user = await getUserOrNull(ctx);
+ *     const session = await db.get(sessionId);
+ *     const db = wrapDatabaseReader({ user }, ctx.db, rlsRules);
+ *     return { ctx: { db, user, session }, args: {} };
+ *   },
+ * });
+ *
+ * // Using the custom builder
+ * export const getSomeData = myActionBuilder({
+ *   args: { someArg: z.string() },
+ *   handler: async (ctx, args) => {
+ *     const { db, user, session, scheduler } = ctx;
+ *     const { someArg } = args;
+ *     // ...
+ *   }
+ * });
+ * ```
+ *
+ * Simple usage only modifying ctx:
+ * ```js
+ * const myInternalAction = zCustomAction(
+ *   internalAction,
+ *   customCtx(async (ctx) => {
+ *     return {
+ *       // Throws an exception if the user isn't logged in
+ *       user: await getUserByTokenIdentifier(ctx),
+ *     };
+ *   })
+ * );
+ *
+ * // Using it
+ * export const getUser = myInternalAction({
+ *   args: { email: z.string().email() },
+ *   handler: async (ctx, args) => {
+ *     console.log(args.email);
+ *     return ctx.user;
+ *   },
+ * });
+ *
+ * @param action The action to be modified. Usually `action` or `internalAction`
+ *   from `_generated/server`.
+ * @param mod The modifier to be applied to the action, changing ctx and args.
+ * @returns A new action builder using zod validation to define queries.
+ */
+export function zCustomAction<
+  ModArgsValidator extends PropertyValidators,
+  ModCtx extends Record<string, any>,
+  ModMadeArgs extends Record<string, any>,
+  Visibility extends FunctionVisibility,
+  DataModel extends GenericDataModel
+>(
+  action: ActionBuilder<DataModel, Visibility>,
+  mod: Mod<GenericActionCtx<DataModel>, ModArgsValidator, ModCtx, ModMadeArgs>
+) {
+  // Looking forward to when input / args / ... are optional
+  const inputMod = mod.input ?? NoOp.input;
+  const inputArgs = mod.args ?? NoOp.args;
+  function customActionBuilder(fn: any): any {
+    if ("args" in fn) {
+      const convexValidator = zodToConvex(fn.args);
+      return action({
+        args: {
+          ...convexValidator,
+          ...inputArgs,
+        },
+        handler: async (ctx, allArgs: any) => {
+          const { split, rest } = splitArgs(inputArgs, allArgs);
+          const added = await inputMod(ctx, split);
+          try {
+            const validated = z.object(fn.args).parse(rest);
+            return await fn.handler(
+              { ...ctx, ...added.ctx },
+              { ...validated, ...added.args }
+            );
+          } catch (e) {
+            if (e instanceof z.ZodError) {
+              throw new ConvexError({
+                ZodError: JSON.parse(JSON.stringify(e.errors, null, 2)),
+              });
+            }
+            throw e;
+          }
+        },
+      });
+    }
+    if (Object.keys(inputArgs).length > 0) {
+      throw new Error(
+        "If you're using a custom function with arguments for the input " +
+          "modifier, you must declare the arguments for the function too."
+      );
+    }
+    const handler = fn.handler ?? fn;
+    return action({
+      handler: async (ctx, args: any) => {
+        const { ctx: modCtx } = await inputMod(ctx, args);
+        return await handler({ ...ctx, ...modCtx }, args);
+      },
+    });
+  }
+
+  return customActionBuilder as CustomBuilder<
+    "action",
+    ModArgsValidator,
+    ModCtx,
+    ModMadeArgs,
+    GenericActionCtx<DataModel>,
     Visibility
   >;
 }
