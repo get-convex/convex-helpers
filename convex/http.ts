0eb7e1fb46bb3a83bb8d4779ee132058b9156758
Ian Macartney <ianhmacartney@gmail.com>
2023-12-13 14:10:17 -0800

    add example http

--- convex/http.ts
+++ convex/http.ts
@@ -4,5 +4,8 @@ import { HonoWithConvex, HttpRouterWithHono } from "./lib/honoWithConvex";
 const app: HonoWithConvex = new Hono();
 
 // See the [Stack post on using Hono](https://stack.convex.dev)
+app.get("/", async (c) => {
+  return c.json("Hello world!");
+});
 
 export default new HttpRouterWithHono(app);
