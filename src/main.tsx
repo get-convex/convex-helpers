81a4b984f01e0a86150848050b32443665d1c7a9
Ian Macartney <ianhmacartney@gmail.com>
2023-12-18 17:34:57 -0800

    update reactdom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
@@ -7,13 +7,12 @@ import { SessionProvider } from "./hooks/useServerSession";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
-ReactDOM.render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ConvexProvider client={convex}>
       <SessionProvider storageLocation={"sessionStorage"}>
         <App />
       </SessionProvider>
     </ConvexProvider>
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
