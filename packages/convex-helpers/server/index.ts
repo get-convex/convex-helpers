93d231fe975da9cb851293dcac12b2199042edec
Ian Macartney <ianhmacartney@gmail.com>
2024-02-04 18:42:07 -0800

    split env var helper to have deployment name

--- packages/convex-helpers/server/index.ts
+++ packages/convex-helpers/server/index.ts
@@ -2,6 +2,9 @@ import { defineTable } from "convex/server";
 import { Validator, v } from "convex/values";
 
 /**
+ * Define a table with system fields _id and _creationTime. This also returns
+ * helpers for working with the table in validators. See:
+ * https://stack.convex.dev/argument-validation-without-repetition#table-helper-for-schema-definition--validation
  *
  * @param name The table name. This should also be used in defineSchema.
  * @param fields Table fields, as you'd pass to defineTable.
@@ -31,17 +34,29 @@ export function Table<
   };
 }
 
+/**
+ *
+ * @param envVarName - The missing environment variable, e.g. OPENAI_API_KEY
+ * @param whereToGet - Where to get it, e.g. "https://platform.openai.com/account/api-keys"
+ * @returns A string with instructions on how to set the environment variable.
+ */
 export function missingEnvVariableUrl(envVarName: string, whereToGet: string) {
-  const deploymentName = process.env.CONVEX_CLOUD_URL?.slice(8).replace(
-    ".convex.cloud",
-    ""
-  );
+  const deployment = deploymentName();
+  if (!deployment) return `Missing ${envVarName} in environment variables.`;
   return (
     `\n  Missing ${envVarName} in environment variables.\n\n` +
-    `  Get it from ${whereToGet} .\n` +
-    "  Paste it on the Convex dashboard:\n" +
-    "  https://dashboard.convex.dev/d/" +
-    deploymentName +
-    `/settings?var=${envVarName}`
+    `  Get it from ${whereToGet} .\n  Paste it on the Convex dashboard:\n` +
+    `  https://dashboard.convex.dev/d/${deployment}/settings?var=${envVarName}`
   );
+}
+
+/**
+ * Get the deployment name from the CONVEX_CLOUD_URL environment variable.
+ * @returns The deployment name, like "screaming-lemur-123"
+ */
+export function deploymentName() {
+  const url = process.env.CONVEX_CLOUD_URL;
+  if (!url) return undefined;
+  const regex = new RegExp("https://(.+).convex.cloud");
+  return regex.exec(url)?.[1];
 }
