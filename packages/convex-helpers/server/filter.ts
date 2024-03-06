073d376fb67042e2ef7395f2918f36e3be1355de
Lee Danilek <lee@convex.dev>
2024-03-05 21:03:27 -0500

    .

--- packages/convex-helpers/server/filter.ts
+++ packages/convex-helpers/server/filter.ts
@@ -3,35 +3,6 @@
  * JavaScript/TypeScript function that filters results just like
  * `db.query(...).filter(...)` but with more generality.
  *
- * Examples:
- * // Full table scan, filtered to short messages.
- * return await filter(
- *  ctx.db.query("messages"),
- *  async (message) => message.body.length < 10,
- * ).collect();
- *
- * // Short messages by author, paginated.
- * return await filter(
- *  ctx.db.query("messages").withIndex("by_author, q=>q.eq("author", args.author)),
- *  async (message) => message.body.length < 10,
- * ).paginate(args.paginationOpts);
- *
- * // Same behavior as above: Short messages by author, paginated.
- * // Note the filter can wrap any part of the query pipeline, and it is applied
- * // at the end. This is how RowLevelSecurity works.
- * const shortMessages = await filter(
- *  ctx.db.query("messages"),
- *  async (message) => message.body.length < 10,
- * );
- * return await shortMessages
- *  .withIndex("by_author, q=>q.eq("author", args.author))
- *  .paginate(args.paginationOpts);
- *
- * // Also works with `order()`, `take()`, `unique()`, and `first()`.
- * return await filter(
- *  ctx.db.query("messages").order("desc"),
- *  async (message) => message.body.length < 10,
- * ).first();
  */
 
 import {
@@ -171,10 +142,46 @@ export type Predicate<T extends GenericTableInfo> = (
 type QueryTableInfo<Q> = Q extends Query<infer T> ? T : never;
 
 /**
+ * Applies a filter to a database query, just like `.filter((q) => ...)` but
+ * supporting arbitrary JavaScript/TypeScript.
+ * Performance is roughly the same as `.filter((q) => ...)`. If you want better
+ * performance, use an index to narrow down the results before filtering.
  *
- * @param query The query
- * @param predicate
- * @returns
+ * Examples:
+ *
+ * // Full table scan, filtered to short messages.
+ * return await filter(
+ *  ctx.db.query("messages"),
+ *  async (message) => message.body.length < 10,
+ * ).collect();
+ *
+ * // Short messages by author, paginated.
+ * return await filter(
+ *  ctx.db.query("messages").withIndex("by_author, q=>q.eq("author", args.author)),
+ *  async (message) => message.body.length < 10,
+ * ).paginate(args.paginationOpts);
+ *
+ * // Same behavior as above: Short messages by author, paginated.
+ * // Note the filter can wrap any part of the query pipeline, and it is applied
+ * // at the end. This is how RowLevelSecurity works.
+ * const shortMessages = await filter(
+ *  ctx.db.query("messages"),
+ *  async (message) => message.body.length < 10,
+ * );
+ * return await shortMessages
+ *  .withIndex("by_author, q=>q.eq("author", args.author))
+ *  .paginate(args.paginationOpts);
+ *
+ * // Also works with `order()`, `take()`, `unique()`, and `first()`.
+ * return await filter(
+ *  ctx.db.query("messages").order("desc"),
+ *  async (message) => message.body.length < 10,
+ * ).first();
+ *
+ * @param query The query to filter.
+ * @param predicate Async function to run on each document before it is yielded
+ *  from the query pipeline.
+ * @returns A new query with the filter applied.
  */
 export function filter<Q extends Query<GenericTableInfo>>(
   query: Q,
