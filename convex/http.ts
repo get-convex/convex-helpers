124da5beea5f5abe10d6c0179897289fe65cf207
Sarah Shader <sarahshader@gmail.com>
2024-03-27 16:24:51 -0400

    wip

--- convex/http.ts
+++ convex/http.ts
@@ -1,7 +1,11 @@
 import { HonoWithConvex, HttpRouterWithHono } from "convex-helpers/server/hono";
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
+import { stream, streamText, streamSSE } from 'hono/streaming'
 import { cors } from "hono/cors";
 import { ActionCtx, query } from "./_generated/server";
+import { FunctionReference, OptionalRestArgs, FunctionReturnType, Scheduler, Auth, StorageActionWriter, TableNamesInDataModel, VectorIndexes, NamedTableInfo, VectorFilterBuilder, DocumentByInfo, NamedVectorIndex, FilterExpression } from "convex/server";
+import { GenericId } from "convex/values";
+import { BlankInput } from "hono/types";
 
 const app: HonoWithConvex<ActionCtx> = new Hono();
 
@@ -11,6 +15,21 @@ app.use("/*", cors());
 app.get("/", async (c) => {
   return c.json("Hello world!");
 });
+app.get("/test", async (c) => {
+  let id = 1;
+  return streamSSE(c, async (stream) => {
+    while (id < 3) {
+      const message = `It is ${new Date().toISOString()}`
+      await stream.writeSSE({
+        data: message,
+        event: 'time-update',
+        id: String(id++),
+      })
+      await stream.sleep(1000)
+    }
+    await stream.close()
+  })
+})
 
 export default new HttpRouterWithHono(app);
 
@@ -23,3 +42,4 @@ export const siteUrl = query({
     return process.env.CONVEX_SITE_URL;
   },
 });
+
