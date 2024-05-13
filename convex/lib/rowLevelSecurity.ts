af959142f1004140ba02543c62980f2581a2d674
Ian Macartney <ianhmacartney@gmail.com>
2024-05-12 21:32:42 -0700

    rls example with addRLS generic fn

--- convex/lib/rowLevelSecurity.ts
+++ convex/lib/rowLevelSecurity.ts
@@ -3,79 +3,46 @@ import {
   customMutation,
   customQuery,
 } from "convex-helpers/server/customFunctions";
-import {
-  DocumentByName,
-  GenericDataModel,
-  GenericQueryCtx,
-  TableNamesInDataModel,
-  WithoutSystemFields,
-} from "convex/server";
-import {
-  internalMutation,
-  internalQuery,
-  mutation,
-  query,
-} from "../_generated/server";
-import { Callbacks, DEFAULT, wrapDB } from "convex-helpers/server/wrapDB";
+import { mutation, MutationCtx, query, QueryCtx } from "../_generated/server";
+import { DEFAULT, wrapDB } from "convex-helpers/server/wrapDB";
 import { DataModel } from "../_generated/dataModel";
 import { getUserByTokenIdentifier } from "./withUser";
 import { v } from "convex/values";
 
 /**
- * If you just want to read from the DB, you can use this.
- * Later, you can use your own custom function using wrapDB.
+ * Example usage:
  */
-export function BasicRowLevelSecurity(rules: Callbacks<DataModel>) {
+export function addRLS<Ctx extends QueryCtx | MutationCtx>(ctx: Ctx) {
   return {
-    queryWithRLS: customQuery(
-      query,
-      customCtx((ctx) => ({ db: wrapDB(ctx, rules) })),
-    ),
-
-    mutationWithRLS: customMutation(
-      mutation,
-      customCtx((ctx) => ({ db: wrapDB(ctx, rules) })),
-    ),
-
-    internalQueryWithRLS: customQuery(
-      internalQuery,
-      customCtx((ctx) => ({ db: wrapDB(ctx, rules) })),
-    ),
-
-    internalMutationWithRLS: customMutation(
-      internalMutation,
-      customCtx((ctx) => ({ db: wrapDB(ctx, rules) })),
-    ),
+    db: wrapDB<DataModel, Ctx>(ctx, {
+      users: async ({ ctx, op, doc }) => {
+        const loggedInUser = await getUserByTokenIdentifier(ctx);
+        switch (op) {
+          case "read":
+            return true;
+          case "create":
+            if (loggedInUser) {
+              return loggedInUser.tokenIdentifier === doc.tokenIdentifier;
+            } else {
+              return (
+                (await ctx.auth.getUserIdentity())?.tokenIdentifier ===
+                doc.tokenIdentifier
+              );
+            }
+          case "update":
+          case "delete":
+            return loggedInUser?._id === doc._id;
+        }
+      },
+      [DEFAULT]: (table) => {
+        throw new Error(`No rule for table ${table}`);
+      },
+    }),
   };
 }
 
-/**
- * Example usage:
- */
-const { queryWithRLS, mutationWithRLS } = BasicRowLevelSecurity({
-  users: async ({ ctx, op, doc }) => {
-    const loggedInUser = await getUserByTokenIdentifier(ctx);
-    switch (op) {
-      case "read":
-        return true;
-      case "create":
-        if (loggedInUser) {
-          return loggedInUser.tokenIdentifier === doc.tokenIdentifier;
-        } else {
-          return (
-            (await ctx.auth.getUserIdentity())?.tokenIdentifier ===
-            doc.tokenIdentifier
-          );
-        }
-      case "update":
-      case "delete":
-        return loggedInUser?._id === doc._id;
-    }
-  },
-  [DEFAULT]: (table) => {
-    throw new Error(`No rule for table ${table}`);
-  },
-});
+const queryWithRLS = customQuery(query, customCtx(addRLS));
+const mutationWithRLS = customMutation(mutation, customCtx(addRLS));
 
 export const getUsers = queryWithRLS({
   args: {},
