69de5281e0105d7c26168e7a15881053e21fca0a
Ian Macartney <ianhmacartney@gmail.com>
2024-05-12 21:36:28 -0700

    rls example returning wrapDB

--- convex/lib/rowLevelSecurity.ts
+++ convex/lib/rowLevelSecurity.ts
@@ -12,37 +12,42 @@ import { v } from "convex/values";
 /**
  * Example usage:
  */
-export function addRLS<Ctx extends QueryCtx | MutationCtx>(ctx: Ctx) {
-  return {
-    db: wrapDB<DataModel, Ctx>(ctx, {
-      users: async ({ ctx, op, doc }) => {
-        const loggedInUser = await getUserByTokenIdentifier(ctx);
-        switch (op) {
-          case "read":
-            return true;
-          case "create":
-            if (loggedInUser) {
-              return loggedInUser.tokenIdentifier === doc.tokenIdentifier;
-            } else {
-              return (
-                (await ctx.auth.getUserIdentity())?.tokenIdentifier ===
-                doc.tokenIdentifier
-              );
-            }
-          case "update":
-          case "delete":
-            return loggedInUser?._id === doc._id;
-        }
-      },
-      [DEFAULT]: (table) => {
-        throw new Error(`No rule for table ${table}`);
-      },
-    }),
-  };
+function addRLS<Ctx extends QueryCtx | MutationCtx>(ctx: Ctx) {
+  return wrapDB<DataModel, Ctx>(ctx, {
+    users: async ({ ctx, op, doc }) => {
+      const loggedInUser = await getUserByTokenIdentifier(ctx);
+      switch (op) {
+        case "read":
+          return true;
+        case "create":
+          if (loggedInUser) {
+            return loggedInUser.tokenIdentifier === doc.tokenIdentifier;
+          } else {
+            return (
+              (await ctx.auth.getUserIdentity())?.tokenIdentifier ===
+              doc.tokenIdentifier
+            );
+          }
+        case "update":
+        case "delete":
+          return loggedInUser?._id === doc._id;
+      }
+    },
+    [DEFAULT]: (table) => {
+      throw new Error(`No rule for table ${table}`);
+    },
+  });
 }
 
-const queryWithRLS = customQuery(query, customCtx(addRLS));
-const mutationWithRLS = customMutation(mutation, customCtx(addRLS));
+const queryWithRLS = customQuery(
+  query,
+  customCtx((ctx) => ({ db: addRLS(ctx) })),
+);
+
+const mutationWithRLS = customMutation(
+  mutation,
+  customCtx((ctx) => ({ db: addRLS(ctx) })),
+);
 
 export const getUsers = queryWithRLS({
   args: {},
