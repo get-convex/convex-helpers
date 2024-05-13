3bd2bbbbb0fb4bebab3314b1a5c1e8bfc90eddaf
Ian Macartney <ianhmacartney@gmail.com>
2024-05-13 01:14:53 -0700

    some stubs of tests for different ways of defining callbacks

--- tests/convex/wrapDB.test.ts
+++ tests/convex/wrapDB.test.ts
@@ -11,12 +11,13 @@ import {
   mutation,
   query,
 } from "./_generated/server";
-import { Callbacks, wrapDB } from "convex-helpers/server/wrapDB";
+import { Callbacks, DEFAULT, wrapDB } from "convex-helpers/server/wrapDB";
 import {
   DataModelFromSchemaDefinition,
   defineSchema,
   defineTable,
   GenericMutationCtx,
+  GenericQueryCtx,
 } from "convex/server";
 
 const schema = defineSchema({
@@ -29,32 +30,81 @@ type DataModel = DataModelFromSchemaDefinition<typeof schema>;
 
 test("wrapReader", async () => {
   const t = convexTest(schema);
-  await t.run(async (ctx: GenericMutationCtx<DataModel>) => {
+  await t.run(async (ctx) => {
     const rules: Callbacks<DataModel> = {
       tableA: ({ ctx, op, doc, update }) => {
         switch (op) {
           case "create":
             doc;
+            ctx;
+            break;
           case "read":
             doc;
+            ctx;
+            break;
           case "update":
+            doc;
+            ctx;
+            update;
             return !!doc;
           case "delete":
+            doc;
+            ctx;
+            break;
         }
+        return true;
       },
     };
     const wrappedCtx = wrapDB(ctx, rules);
-    const wrappedCtx2 = wrapDB(ctx, {
+    const wrappedCtx2 = wrapDB(
+      ctx as GenericQueryCtx<DataModel>,
+      {
+        tableA: ({ ctx, op, doc, update }) => {
+          switch (op) {
+            case "create":
+              ctx;
+              doc;
+              break;
+            case "read":
+              ctx;
+              doc;
+              break;
+            case "update":
+              ctx;
+              doc;
+              break;
+            case "delete":
+              ctx;
+              doc;
+              break;
+          }
+          return true;
+        },
+        [DEFAULT]: async ({ ctx, op, doc, update }) => true,
+      } as Callbacks<DataModel>,
+    );
+    const wrappedCtx3 = wrapDB<DataModel>(ctx, {
       tableA: ({ ctx, op, doc, update }) => {
         switch (op) {
           case "create":
             doc;
+            ctx;
+            break;
           case "read":
             doc;
+            ctx;
+            break;
           case "update":
+            doc;
+            ctx;
+            update;
             return !!doc;
           case "delete":
+            doc;
+            ctx;
+            break;
         }
+        return true;
       },
     });
   });
