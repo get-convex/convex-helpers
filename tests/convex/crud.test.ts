ed50885fc68d0b93fa3b86189da2189ee6fc0922
Ian Macartney <ianhmacartney@gmail.com>
2024-07-11 17:09:27 -0700

    fix crud to be self-standing

--- tests/convex/crud.test.ts
+++ tests/convex/crud.test.ts
@@ -1,23 +1,64 @@
 import { convexTest } from "convex-test";
 import { expect, test } from "vitest";
-import schema from "./schema";
-import { internal } from "./_generated/api";
+import { crud } from "convex-helpers/server";
+import {
+  anyApi,
+  ApiFromModules,
+  defineSchema,
+  defineTable,
+} from "convex/server";
+import { v } from "convex/values";
+import { internalQueryGeneric, internalMutationGeneric } from "convex/server";
+import { modules } from "./setup.test";
+
+const ExampleFields = {
+  foo: v.string(),
+  bar: v.union(v.object({ n: v.optional(v.number()) }), v.null()),
+  baz: v.optional(v.boolean()),
+};
+const CrudTable = "crud_example";
+
+export const { create, read, paginate, update, destroy } = crud(
+  // We could use the Table helper instead, but showing it explicitly here.
+  // E.g. Table("crud_example", ExampleFields)
+  {
+    name: CrudTable,
+    _id: v.id(CrudTable),
+    withoutSystemFields: ExampleFields,
+  },
+  internalQueryGeneric,
+  internalMutationGeneric,
+);
+
+const schema = defineSchema({
+  [CrudTable]: defineTable(ExampleFields),
+});
+
+const testApi: ApiFromModules<{
+  fns: {
+    create: typeof create;
+    read: typeof read;
+    update: typeof update;
+    paginate: typeof paginate;
+    destroy: typeof destroy;
+  };
+}>["fns"] = anyApi["crud.test"] as any;
 
 test("crud for table", async () => {
-  const t = convexTest(schema);
-  const doc = await t.mutation(internal.crud.create, { foo: "", bar: null });
+  const t = convexTest(schema, modules);
+  const doc = await t.mutation(testApi.create, { foo: "", bar: null });
   expect(doc).toMatchObject({ foo: "", bar: null });
-  const read = await t.query(internal.crud.read, { id: doc._id });
+  const read = await t.query(testApi.read, { id: doc._id });
   expect(read).toMatchObject(doc);
-  await t.mutation(internal.crud.update, {
+  await t.mutation(testApi.update, {
     id: doc._id,
     patch: { foo: "new", bar: { n: 42 }, baz: true },
   });
-  expect(await t.query(internal.crud.read, { id: doc._id })).toMatchObject({
+  expect(await t.query(testApi.read, { id: doc._id })).toMatchObject({
     foo: "new",
     bar: { n: 42 },
     baz: true,
   });
-  await t.mutation(internal.crud.destroy, { id: doc._id });
-  expect(await t.query(internal.crud.read, { id: doc._id })).toBe(null);
+  await t.mutation(testApi.destroy, { id: doc._id });
+  expect(await t.query(testApi.read, { id: doc._id })).toBe(null);
 });
--- tests/convex/schema.ts
+++ tests/convex/schema.ts
@@ -1,10 +1,8 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
-import { crudExampleTables } from "./crud";
 
 export default defineSchema({
   users: defineTable({
     tokenIdentifier: v.string(),
   }).index("tokenIdentifier", ["tokenIdentifier"]),
-  ...crudExampleTables,
 });
