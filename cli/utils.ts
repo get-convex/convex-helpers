01e60269c90d996e9ea59922ac82cad9c9012c5b
Jordan Hunt <jordan@convex.dev>
2024-10-30 16:25:54 -0700

    impl

--- cli/utils.ts
+++ cli/utils.ts
@@ -1,4 +1,6 @@
 import fs from 'fs'
+import path from 'path'
+import os from 'os'
 import { execSync } from 'child_process';
 import chalk from 'chalk';
 import { ValidatorJSON } from 'convex/values';
@@ -20,30 +22,30 @@ export type AnalyzedFunction = {
     returns: ValidatorJSON | null
 };
 
-export function getFunctionSpec(prod?: boolean, filePath?: string) {
-    if (filePath && prod) {
+export function getFunctionSpec(prod?: boolean, filePathOpt?: string) {
+    if (filePathOpt && prod) {
         console.error(`To use the prod flag, you can't provide a file path`);
         process.exit(1)
     }
     let content: string;
-    if (filePath && !fs.existsSync(filePath)) {
-        console.error(chalk.red(`File ${filePath} not found`));
+    if (filePathOpt && !fs.existsSync(filePathOpt)) {
+        console.error(chalk.red(`File ${filePathOpt} not found`));
         process.exit(1);
     }
-    if (filePath) {
-        content = fs.readFileSync(filePath, 'utf-8');
-    } else {
-        const flags = prod ? '--prod' : '';
-        let output: string;
+    let filePath = filePathOpt ?? path.join(os.tmpdir(), 'function-spec.json');
+    if (!filePathOpt) {
         try {
-            output = execSync(`npx convex function-spec ${flags}`).toString();
+            const flags = prod ? "--prod" : "";
+            execSync(`npx convex function-spec ${flags} > ${filePath}`).toString();
         } catch (e) {
+            console.log(e);
             console.error(chalk.red("\nError retrieving function spec from your Convex deployment. " +
                 "Confirm that you \nare running this command from within a Convex project.\n"));
             process.exit(1);
         }
-        content = output.toString();
     }
+    content = fs.readFileSync(filePath, 'utf-8');
+    fs.unlinkSync(filePath);
 
     return content;
 }
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,5 +1,3 @@
-/* prettier-ignore-start */
-
 /* eslint-disable */
 /**
  * Generated `api` utility.
@@ -56,5 +54,3 @@ export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-/* prettier-ignore-end */
