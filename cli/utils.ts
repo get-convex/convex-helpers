65b900dddf6305a6c5cf1d59f845ebb90090be5e
Jordan Hunt <jordan@convex.dev>
2024-11-08 14:24:56 -0800

    code review

--- cli/utils.ts
+++ cli/utils.ts
@@ -1,50 +1,55 @@
-import fs from 'fs'
-import path from 'path'
-import os from 'os'
-import { execSync } from 'child_process';
-import chalk from 'chalk';
-import { ValidatorJSON } from 'convex/values';
+import fs from "fs";
+import { execSync } from "child_process";
+import chalk from "chalk";
+import { ValidatorJSON } from "convex/values";
 
-type Visibility = { kind: 'public' } | { kind: 'internal' };
+type Visibility = { kind: "public" } | { kind: "internal" };
 
-type FunctionType = 'Action' | 'Mutation' | 'Query' | 'HttpAction';
+type FunctionType = "Action" | "Mutation" | "Query" | "HttpAction";
 
 export type FunctionSpec = {
-    url: string,
-    functions: AnalyzedFunction[]
+  url: string;
+  functions: AnalyzedFunction[];
 };
 
 export type AnalyzedFunction = {
-    identifier: string
-    functionType: FunctionType
-    visibility: Visibility
-    args: ValidatorJSON | null
-    returns: ValidatorJSON | null
+  identifier: string;
+  functionType: FunctionType;
+  visibility: Visibility;
+  args: ValidatorJSON | null;
+  returns: ValidatorJSON | null;
 };
 
-export function getFunctionSpec(prod?: boolean, filePathOpt?: string) {
-    if (filePathOpt && prod) {
-        console.error(`To use the prod flag, you can't provide a file path`);
-        process.exit(1)
+export function getFunctionSpec(prod?: boolean, filePath?: string) {
+  if (filePath && prod) {
+    console.error(`To use the prod flag, you can't provide a file path`);
+    process.exit(1);
+  }
+  let content: string;
+  if (filePath && !fs.existsSync(filePath)) {
+    console.error(chalk.red(`File ${filePath} not found`));
+    process.exit(1);
+  }
+  if (filePath) {
+    content = fs.readFileSync(filePath, "utf-8");
+  } else {
+    const flags = prod ? "--prod" : "";
+    let output: string;
+    try {
+      output = execSync(`npx convex function-spec ${flags}`, {
+        maxBuffer: 1024 * 1024 * 10 /* 10 MB */,
+      }).toString();
+    } catch (e) {
+      console.error(
+        chalk.red(
+          "\nError retrieving function spec from your Convex deployment. " +
+            "Confirm that you \nare running this command from within a Convex project.\n",
+        ),
+      );
+      process.exit(1);
     }
-    let content: string;
-    if (filePathOpt && !fs.existsSync(filePathOpt)) {
-        console.error(chalk.red(`File ${filePathOpt} not found`));
-        process.exit(1);
-    }
-    let filePath = filePathOpt ?? path.join(os.tmpdir(), 'function-spec.json');
-    if (!filePathOpt) {
-        try {
-            const flags = prod ? "--prod" : "";
-            execSync(`npx convex function-spec ${flags} > ${filePath}`).toString();
-        } catch (e) {
-            console.error(chalk.red("\nError retrieving function spec from your Convex deployment. " +
-                "Confirm that you \nare running this command from within a Convex project.\n"));
-            process.exit(1);
-        }
-    }
-    content = fs.readFileSync(filePath, 'utf-8');
-    fs.unlinkSync(filePath);
+    content = output.toString();
+  }
 
-    return content;
+  return content;
 }
