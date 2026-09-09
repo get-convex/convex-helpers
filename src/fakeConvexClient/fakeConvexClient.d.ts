d17aad478e563f695e78ab7350861ef4f1ebf462
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-09-09 14:41:34 -0700

    use official type helpers

--- src/fakeConvexClient/fakeConvexClient.d.ts
+++ src/fakeConvexClient/fakeConvexClient.d.ts
@@ -1,15 +1,19 @@
 import { ConvexReactClient } from "convex/react";
-import { FunctionReference } from "convex/server";
+import {
+  FunctionReference,
+  FunctionArgs,
+  FunctionReturnType,
+} from "convex/server";
 
 export class ConvexReactClientFake extends ConvexReactClient {
   constructor();
 
   registerQueryFake<FuncRef extends FunctionReference<"query", "public">>(
     funcRef: FuncRef,
-    impl: (args: FuncRef["_args"]) => FuncRef["_returnType"],
+    impl: (args: FunctionArgs<FuncRef>) => FunctionReturnType<FuncRef>,
   ): void;
   registerMutationFake<FuncRef extends FunctionReference<"mutation", "public">>(
     funcRef: FuncRef,
-    impl: (args: FuncRef["_args"]) => FuncRef["_returnType"],
+    impl: (args: FunctionArgs<FuncRef>) => FunctionReturnType<FuncRef>,
   ): void;
 }
